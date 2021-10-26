e51047d8d5bc940b28cea345d7cc5cb1caeae1e6
Dalila K Leo <dalilakatialeo@gmail.com>
2021-10-26 21:46:23 +1000

    styling and assets

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,17 +1,23 @@
 // npm modules from https://npm.js.com/
 import React from "react"
 import { Link } from "react-router-dom"
+// import LoginForm from "../LoginForm/LoginForm"
+import "./Nav.css"
 
+
+// something to handle authentication and only show new project + if user is logged in
 
 const Nav = () => {
   return (
     <div>
-      <nav>
+      <nav className="navbar">
         <Link to="/">Home</Link>
         <Link to="/login/">Login</Link>
-        <Link to="/new-project/">Create new project</Link>
+        <Link to="/new-project/">New project +</Link>
       </nav>
-      <img id="logo" src={"locally-logo.jpg"} alt="locally"/>
+      <div id="img-div">
+        <img id="logo" src={"locally-logo.jpg"} alt="locally"/>
+      </div>
     </div>
   )
 }
