29232c899b63155d608cf394b3ead359f7282ef3
Dalila K Leo <dalilakatialeo@gmail.com>
2021-10-27 16:25:49 +1000

    new styling and authentication

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,6 +1,6 @@
 // npm modules from https://npm.js.com/
 import React from "react"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 // import LoginForm from "../LoginForm/LoginForm"
 import "./Nav.css"
 
@@ -8,12 +8,19 @@ import "./Nav.css"
 // something to handle authentication and only show new project + if user is logged in
 
 const Nav = () => {
+  const location = useLocation()
+  // console.log(location)
   return (
     <div>
       <nav className="navbar">
-        <Link to="/">Home</Link>
-        <Link to="/login/">Login</Link>
-        <Link to="/new-project/">New project +</Link>
+        <Link className={`${location.pathname === '/' ? 'active' : ''}`}to="/">Home</Link>
+        
+        {
+          localStorage.getItem('token') 
+          ? <Link className={`${location.pathname === '/new-project/' ? 'active' : ''}`}to="/new-project/">New project +</Link>
+          : <Link className={`${location.pathname === '/login/' ? 'active' : ''}`} to="/login/">Login</Link>
+
+        }
       </nav>
       <div id="img-div">
         <img id="logo" src={"locally-logo.jpg"} alt="locally"/>
