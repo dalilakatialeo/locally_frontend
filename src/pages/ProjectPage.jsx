1c2ac3b08e6b36004dcaee5ecd400e2eb022fb19
Dalila K Leo <dalilakatialeo@gmail.com>
2021-11-03 17:58:15 +1000

    Update ProjectPage.jsx

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -53,9 +53,9 @@ const deleteFunction = async () => {
     return (
         <div>
             <h2>{projectData?.title}</h2>
+            <h3>Location: {projectData?.location}</h3>
             <h3>Created: {formattedDate}</h3>
             <h3>{projectData?.description}</h3>
-            <h3>Location: {projectData?.location}</h3>
             <h3>Goal: $ {projectData?.goal}.00</h3>
             <img src= {projectData?.image}/>
             {/* <h3>{`Active: ${active()}`}</h3> */}
